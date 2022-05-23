d830d0d01b4ed74742484148a079e22eaabaca65
开关 <gltwork@163.com>
2022-05-23 17:20:51 +0800

    add footer

--- src/components/GViewCard/index.vue
+++ src/components/GViewCard/index.vue
@@ -1,14 +1,43 @@
 <template>
   <div class="g-view-card">
-    <div class="g-view-content">
+    <div
+      class="g-view-content"
+      :style="{
+        height: showFooter ? 'calc(100% - 68px)' : '',
+        'padding-bottom': showFooter ? '48px' : '',
+      }"
+    >
       <slot />
+    </div>
+    <div v-if="showFooter" class="g-view-card-footer">
+      <el-button
+        :loading="saveLoad"
+        size="large"
+        type="primary"
+        @click="() => this.$emit('success')"
+      >
+        保 存
+      </el-button>
+      <el-button size="large" @click="() => this.$emit('cancel')">
+        取 消
+      </el-button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "index",
+  name: "GViewCard",
+  props: {
+    showFooter: {
+      type: Boolean,
+      default: false,
+    },
+    saveLoad: {
+      type: Boolean,
+      default: false,
+    },
+  },
 };
 </script>
 
@@ -18,6 +47,8 @@ export default {
   height: 100%;
   padding: 12px 0 12px 12px;
   overflow-y: auto;
+  position: relative;
+  overflow-y: auto;
   .g-view-content {
     width: calc(100% - 12px);
     height: 100%;
@@ -26,6 +57,22 @@ export default {
     box-shadow: 2px 0 7px 0 rgb(153, 170, 192);
     border-radius: 4px;
     background: #fff;
+  }
+  .g-view-card-footer {
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 64px;
+    background: #fff;
+    box-shadow: 2px 0 7px 0 rgb(153, 170, 192);
+    display: flex;
+    align-items: center;
+    padding: 0 24px;
+    .ant-btn {
+      width: 120px;
+      margin-right: 16px;
+    }
   }
 }
 </style>
